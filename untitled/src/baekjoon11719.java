ec6f7822a6733fb5d45b851aed75dd7b1299143d
asdfqwerzxcc <wowls7990@gmail.com>
2024-03-21 19:38:46 +0900

    그대로 출력하기2

---
+++ untitled/src/baekjoon11719.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class baekjoon11719 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        String s;
+        while ((s=br.readLine()) !=null){
+            System.out.println(s);
+        }
+
+    }
+}
