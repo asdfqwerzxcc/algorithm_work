d924f9afd04d899ab0d7f0b7a9f8999632d32d2b
asdfqwerzxcc <wowls7990@gmail.com>
2024-04-08 20:59:31 +0900

    주사위 세개

---
+++ untitled/src/beakjoon2480.kt
@@ -0,0 +1,17 @@
+fun main(){
+    val input: List<Int> =readLine()!!.split(" ").map { it.toInt() }
+    var total=0
+    if(input[0]==input[1] &&input[0]==input[2]){
+        total= 10000+input[1]*1000
+    }
+    else if(input[0]==input[1] && input[0]==input[2]){
+        total=1000+input[0]*100
+    }
+    else if(input[1]==input[2]){
+        total=1000+input[1]*100
+    }
+    else{
+        total= input.max()*100
+    }
+    print("$total")
+}
