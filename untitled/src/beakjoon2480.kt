cc0ae4e4924035ae6097954001354bc8f5cbb7ef
asdfqwerzxcc <wowls7990@gmail.com>
2024-04-08 21:00:07 +0900

    주사위 세개

--- untitled/src/beakjoon2480.kt
+++ untitled/src/beakjoon2480.kt
@@ -4,7 +4,7 @@ fun main(){
     if(input[0]==input[1] &&input[0]==input[2]){
         total= 10000+input[1]*1000
     }
-    else if(input[0]==input[1] && input[0]==input[2]){
+    else if(input[0]==input[1] || input[0]==input[2]){
         total=1000+input[0]*100
     }
     else if(input[1]==input[2]){
