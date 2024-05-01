d955931a2be330aefcc32ae94ffb7b1f5b1c480f
asdfqwerzxcc <wowls7990@gmail.com>
2024-05-01 19:22:40 +0900

    오늘의 날짜

---
+++ untitled/src/beakjoon10699.kt
@@ -0,0 +1,8 @@
+import java.time.LocalDate
+import java.time.LocalDateTime
+
+fun main(){
+    val dateAndTime: LocalDateTime = LocalDateTime.now()
+    val onlyDate:LocalDate=LocalDate.now()
+    println(onlyDate)
+}
