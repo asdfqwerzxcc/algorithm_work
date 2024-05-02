6e6950e62097914ceb57cbd7fac29580563e5e40
asdfqwerzxcc <wowls7990@gmail.com>
2024-05-02 21:04:56 +0900

    개표

---
+++ untitled/src/beakjoon10102.kt
@@ -0,0 +1,7 @@
+fun main() {
+    readln()
+    val line = readln()
+    val a = line.count { it == 'A' }
+    val b = line.count { it == 'B' }
+    println(if (a > b) 'A' else if (a < b) 'B' else "Tie")
+}
