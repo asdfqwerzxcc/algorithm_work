71248a97b97b50940b954089e8c4d14f7f8f936e
asdfqwerzxcc <wowls7990@gmail.com>
2024-05-13 20:29:29 +0900

    회의실배정

--- untitled/src/beakjoon1931.kt
+++ untitled/src/beakjoon1931.kt
@@ -8,9 +8,23 @@ fun main() {
 
     val meetings = mutableListOf<Pair<Int, Int>>()
     repeat(n) {
-        val (start, end) = br.readLine.split(" ").map { it.toInt() }
+        val (start, end) = br.readLine().split(" ").map { it.toInt() }
         meetings.add(start to end)
     }
 
+    val sortedMeetings = meetings.sortedBy { it.first }.sortedBy { it.second }
+
+    // 결과 : maxMeetingCnt, 최대 회의의 수
+    var maxMeetingCnt = 0
+    var endTime = 0
+
+    for (meeting in sortedMeetings) {
+        if (meeting.first >= endTime) {
+            endTime = meeting.second
+            maxMeetingCnt++
+        }
+    }
+
+    println(maxMeetingCnt)
     br.close()
 }
