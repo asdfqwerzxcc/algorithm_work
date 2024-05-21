816d052c0dea32b2fc065c5eba30bd2464b893d6
asdfqwerzxcc <wowls7990@gmail.com>
2024-05-21 20:51:34 +0900

    파일정리

---
+++ untitled/src/beakjoon20291.kt
@@ -0,0 +1,14 @@
+fun main(){
+    val extentionList = mutableMapOf<String,Int>()
+    repeat(readln().toInt()){
+        val extension= readln().split(".")[1]
+        if(extentionList.containsKey(extension)) {
+            extentionList[extension] = extentionList[extension]!!
+        }else{
+            extentionList.put(extension,1)
+        }
+    }
+    extentionList.toSortedMap().forEach{
+        println("${it.key} ${it.value}")
+    }
+}
