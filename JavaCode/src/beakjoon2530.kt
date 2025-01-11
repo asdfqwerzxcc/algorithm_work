bbe615ec85ba7be871defb983c1f1f51c47ddf7f
asdfqwerzxcc <wowls7990@gmail.com>
2025-01-11 22:04:55 +0900

    백준 2530 인공지능 시계

---
+++ JavaCode/src/beakjoon2530.kt
@@ -0,0 +1,19 @@
+import java.util.StringTokenizer
+
+fun main(){
+    val st = StringTokenizer(readln())
+    var a= st.nextToken().toInt()
+    var b= st.nextToken().toInt()
+    var c= st.nextToken().toInt()
+    var d= st.nextToken().toInt()
+
+    c+=d
+
+    b+=c/60
+    c%=60
+    a+=b/60
+    b%60
+    a%=24
+
+    println("$a $b $c")
+}
